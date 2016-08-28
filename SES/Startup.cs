aff5426944c674ac0d82497d1c9320087d0ed904
Adrian Khor <adriank96@outlook.com>
2016-08-28 22:41:38 +0800

    Add project files.

---
+++ SES/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SES.Startup))]
+namespace SES
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
